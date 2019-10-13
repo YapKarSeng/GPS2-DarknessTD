07b19bc392bd6d73a6f6f8aa8f95eb4927ed0899
gt8472 <0123706@kdu-online.com>
2019-10-14 01:54:52 +0800

    Map Built

---
+++ Tower Defence 9.29/Assets/Scripts/WinCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinCheck : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (PlayerStats.Lives <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
