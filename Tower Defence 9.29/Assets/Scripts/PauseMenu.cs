1e5b1e3071739e6a65022a6a5f4f8c45cc441897
Karseng <karseng219@gmail.com>
2019-10-21 17:48:31 +0800

    LevelSelectedMenu, CreditMenu, BuildMenu MoneyImpport

--- Tower Defence 9.29/Assets/Scripts/PauseMenu.cs
+++ Tower Defence 9.29/Assets/Scripts/PauseMenu.cs
@@ -48,6 +48,6 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 }
